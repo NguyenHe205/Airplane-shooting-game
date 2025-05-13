01c7cb54f0ec211bee610b05270a4ecdc494c709
NguyenHe205 <hnguyen30052005@gmail.com>
2025-05-13 20:53:32 +0700

    Initial commit

--- Explosion.cpp
+++ Explosion.cpp
@@ -3,16 +3,15 @@
 
 Explosion::Explosion(SDL_Renderer* renderer, int x, int y)
     : renderer(renderer), texture(nullptr), currentFrame(0), totalFrames(4), frameWidth(72), frameHeight(88), frameDelay(8), frameCounter(0) {
-    // Tải hình ảnh sprite sheet của vụ nổ
+
     texture = IMG_LoadTexture(renderer, "explosion-removebg-preview.png");
     if (!texture) {
         SDL_Log("Failed to load explosion.png: %s", IMG_GetError());
     }
 
-    // Khởi tạo srcRect (hình ảnh đầu tiên trong sprite sheet)
+
     srcRect = {0, 0, frameWidth, frameHeight};
 
-    // Khởi tạo dstRect (vị trí vẽ trên màn hình)
     dstRect = {x - frameWidth / 2, y - frameHeight / 2, frameWidth, frameHeight};
 }
 
@@ -28,14 +27,14 @@ void Explosion::update() {
         frameCounter = 0;
         currentFrame++;
         if (currentFrame < totalFrames) {
-            // Chuyển sang frame tiếp theo trong sprite sheet
+
             srcRect.x = currentFrame * frameWidth;
         }
     }
 }
 
 void Explosion::render() {
-    if (currentFrame < totalFrames) {
+    if (currentFrame < totalFrames && texture) {
         SDL_RenderCopy(renderer, texture, &srcRect, &dstRect);
     }
 }
